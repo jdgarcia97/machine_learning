1afa660a9aefc7839f9aa8f2d1cca870862fafe6
jdgarcia97 <jdgarcia97@protonmail.com>
2019-10-20 17:58:36 -0500

    added some comments to the computeCost.m file.

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -12,8 +12,14 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+% Make a prediction from our Matrix X and our vectors of 
+% theta, 
 	predictions = X * theta;
+
+% create a vector of errors using element wise 
+% squaring after we subtract out vector of true values y.
 	errors = (predictions - y) .^2;
+% Sum out errors and divide my 1/2 * m, number of training samples. 
 	J = 1/(2 * m) * sum(errors);
 
 	
